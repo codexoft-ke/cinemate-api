6d7f02d659006fa76e9e0e7192143991908912e9
CodeXoft KE <codexoftke@gmail.com>
2025-09-30 16:45:12 +0300

    fixxes

--- apps/users/models.py
+++ apps/users/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-def create_superuser(self, email_address, password=None, **extra_fields):
+    def create_superuser(self, email_address, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_verified', True)
